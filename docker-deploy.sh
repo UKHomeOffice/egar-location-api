acb687160c7ff58df026d9367daad262f5c3cd9a
Bill Hart <Bill.Hart@civica.co.uk>
2018-02-05 14:04:11 +0000

    Amended error in deploy script

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -3,7 +3,7 @@ NAME="${1}"
 version=$(./scripts/bash/get-site-version.sh)
 
 docker login -u="ukhomeofficedigital+egar_robot" -p=${DOCKER_PASSWORD} quay.io
-docker tag $NAME:$version quay.iod/ukhomeofficedigital/$NAME:$version
+docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:$version
 docker push quay.io/ukhomeofficedigital/$NAME:$version
 
 docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:latest
